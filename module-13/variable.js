f3ef996a45162ff524710a9c5f0acd0a9d5026b8
Ayan Sujon <ayansujonbd@gmail.com>
2025-07-28 23:14:37 +0600

    added variable.js file

---
+++ module-13/variable.js
@@ -0,0 +1,33 @@
+/*
+5 things to declare a variable 
+
+Example: var weight = 38;
+Javascript keyword (var) and Variable Name (Anything related) and (=) Equel Sign and Value and (;) Semicolon
+
+3 Version of Variable: var, let, const
+
+*/
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 500;
+
+
+var age;
+console.log(age);
+console.log(money);
+console.log(price);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
