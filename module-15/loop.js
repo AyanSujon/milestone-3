f49819126e9888d0dd6d2f24e502f479d608ecd4
Ayan <sujon.seo.bd@gmail.com>
2025-07-29 20:56:18 +0600

    added some code in loop.js file

--- module-15/loop.js
+++ module-15/loop.js
@@ -1,1 +1,24 @@
 // What is loop, explore for loop 
+
+// 3 Type of Variable: 
+// const: value will not change
+// let: value can chage
+// var: we should not use it
+
+
+// Variable
+const amount = 200;
+
+// condation 
+if(amount > 100){
+
+}
+else{
+
+}
+
+
+
+// array
+const number = [12, 32, 54, 65];
+
