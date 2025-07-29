61b4241d8bd22b65c16c19b1f58bc5892361a4b3
Ayan <sujon.seo.bd@gmail.com>
2025-07-29 17:10:40 +0600

    added some code

---
+++ module-13/number.js
@@ -0,0 +1,25 @@
+// Know the Fundamentals of js Numbers 
+
+var orangePrice = 20; 
+var choclate = 56.5652;
+var applePrice = '26';
+var total = orangePrice + choclate;
+
+
+// // স্টিং এর ভিতরের নাম্বার কে যদি নাম্বার এ কনভার্ট করতে হয় তাহলে ParseInt('20')
+// var applePrice2 = ParseInt('20');
+// // var applePrice3 = ParseInt(22.4);
+
+// // স্টিং এর ভিতরের দশমির এর পরের নাম্বার কে যদি Print করতে হয় তাহলে ParseFloat('22.5')
+// var applePrice3 = ParseFloat('22.5');
+
+
+// // console.log(typeof orangePrice);
+// // console.log(typeof applePrice);
+
+// console.log(orangePrice + applePrice);
+
+console.log(total.toFixed(2))
+
+
+
