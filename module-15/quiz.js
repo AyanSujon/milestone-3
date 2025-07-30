6f807cd44db972f63d4cdf0d6a9c89dc19b79f21
Ayan <sujon.seo.bd@gmail.com>
2025-07-30 17:25:34 +0600

    added quiz.js file

---
+++ module-15/quiz.js
@@ -0,0 +1,22 @@
+// for loop
+// for(let i = 0; i < 5; i+=3){
+//     console.log(i);
+// }
+
+// // while loop
+// let i = 0;
+// while (i < 5){
+//     console.log(i);
+//     i++;
+// }
+
+//while loop
+let count = 0;
+while (count < 3){
+    console.log("Hello");
+    count++;
+}
+
+
+
+
