d9dbf3df43b29a000bf997e951c21118636478a9
Ayan <sujon.seo.bd@gmail.com>
2025-07-30 13:29:07 +0600

    added for loop

--- module-15/for-sum.js
+++ module-15/for-sum.js
@@ -1,5 +1,21 @@
-// for sum 
-for(let i = 11; i<+20; i++){
-    const sum = 0; 
-    sum = sum + i;
+// // for sum (increment 1 loop)
+// let sum = 0; 
+// for(let i = 11; i<=20; i++){
+//     sum = sum + i;
+//     console.log(i)
+// }
+// console.log('sum of number from 11 to 20 is:', sum);
+
+// for sum (drecment 1 loop)
+// for(let i = 10; i>=0; i--){
+//     // sum = sum + i;
+//     console.log(i);
+// }
+
+// =================================
+// While loop
+let n = 10;
+while(n >=0){
+    console.log(n);
+    n--;
 }
