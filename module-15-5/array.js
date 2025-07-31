eaa67620a105bc2e8553af0bf1f496a7bc68aa5a
Ayan <sujon.seo.bd@gmail.com>
2025-07-31 13:05:20 +0600

    added some code

---
+++ module-15-5/array.js
@@ -0,0 +1,33 @@
+// Array Javascipt
+
+// this is an array 
+const numbers = [12, 23, 52, 65, 89, 52, 85]
+
+// check anumber or array length by length methord
+// console.log(numbers.length);
+
+// console.log(numbers);
+
+// check Array Index Number
+// console.log(numbers[1]);
+
+// Chaek Array index number by vaiable
+const fourth = numbers[4];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
