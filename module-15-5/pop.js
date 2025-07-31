bb36365aa69c1cc287ec3985e54594a0dc3eb3ab
Ayan <sujon.seo.bd@gmail.com>
2025-07-31 17:44:29 +0600

    added pop.js file

---
+++ module-15-5/pop.js
@@ -0,0 +1,31 @@
+
+// Array 
+
+// const friends = ['balam', 'kalam', 'salam', 'gelam' ];
+
+
+
+// friends.pop();
+// console.log(friends);
+
+
+// const out1 = friends.pop();
+// const out2 = friends.pop();
+
+// console.log(out1, out2);
+
+
+const friends = ['balam', 'kalam', 'salam', 'gelam' ];
+// friends.shift();
+friends.unshift('gumailam');
+console.log(friends);
+
+
+
+
+
+
+ 
+
+
+
