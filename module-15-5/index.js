886ea6b993d0b698179cd86c125771de7ae93076
Ayan <sujon.seo.bd@gmail.com>
2025-07-31 17:15:27 +0600

    added index.js file

---
+++ module-15-5/index.js
@@ -0,0 +1,19 @@
+
+// Index Check of an array
+const numbers = [12, 23, 52, 65, 89, 52, 85];
+
+// console.log(numbers);
+// console.log(numbers[1]);
+
+console.log(numbers);
+
+numbers[1] = 999;
+console.log(numbers);
+
+
+
+
+
+
+
+
