954d5a86a4dc38da6962b33207d301c21f9e9b23
Ayan <sujon.seo.bd@gmail.com>
2025-08-01 17:24:43 +0600

    added quiz.js file

---
+++ module-16/quiz.js
@@ -0,0 +1,37 @@
+
+// Check Index of a Variable
+// let str = "Mississippi";
+// console.log(str.indexOf(3));
+
+// check Object
+// const myObject = {
+//     a: 1, 
+//     b: 2,
+//     c: 3
+// }
+
+// for (let prop in myObject){
+//     console.log(myObject[prop]);
+// }
+
+
+// // if else Statement  
+// const x = "";
+// if(!x.length){
+//     console.log("Hey");
+// }else{
+//     console.log("Hi");
+// }
+
+
+// // Convert Uppercase to Lowercase 
+// const str = "Hello";
+// str[0] = "h";
+// console.log(str);
+
+
+var person = {};
+person.name = "John";
+person.age = 30;
+
+
