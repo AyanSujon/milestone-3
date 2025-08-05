c2422a18d95f08f31d0dae5377c2c458983a28dc
Ayan <sujon.seo.bd@gmail.com>
2025-08-05 18:43:47 +0600

    added Module-17-5

---
+++ module-17-5/function.js
@@ -0,0 +1,16 @@
+function hello(){
+    console.log('i am a Function');
+}
+
+hello();
+
+
+// =======================================
+
+function moneybag(){
+    var money = 4500;
+    console.log('I have ' + money + ' Taka.');
+    return; 
+}
+
+moneybag();
